3c7224ed39b90a1c2d7398fd33d33c0216c4c23f
Jared Doval <jared.doval@patelhs.org>
2020-09-28 01:01:31 +0000

    I out that "I am" + myAge + "years old"); and then put that my birthday is 04

--- Main.java
+++ Main.java
@@ -2,5 +2,12 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
   System.out.println("Awesome job!:)");
-}
+  myAge= 15;
+int ("I am" + myAge + "years old");
+myBirthday = 04
+int ("I was born on" + "myBirthday");
+todaysDate = 9/27/2020
+int ("Today's date is" + "todaysDate")
+  }
+ }
 }
